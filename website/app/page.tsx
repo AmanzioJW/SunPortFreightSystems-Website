21256b2d018e22c284c61732de7ebf5e51d7f9e0
AmanzioJW <richard@shopjandw.com>
2025-11-25 21:44:37 -0500

    dev

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,13 +1,18 @@
+import Header from './components/header';
+import HeroSection from './components/herosection';
+import WhyChooseUs from './components/whychooseus';
+import Solutions from './components/services';
+import NewsAndResources from './components/newsandresources';
+import Footer from './components/footer';
 export default function Home() {
   return (
     <div>
+        <Header />
         <HeroSection />
-        <Services />
-        <Industries />
+        <WhyChooseUs />
         <Solutions />
-        <Carriers />
-        <SunPortHub />
-        <AboutUs />
+        <Solutions-CTA />
+        <NewsAndResources />
+        <Footer />
     </div>
   );
-}
