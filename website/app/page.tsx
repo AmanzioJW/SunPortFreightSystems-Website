bb132ee11730f16faedebc5fce47d80be61a0c06
AmanzioJW <richard@shopjandw.com>
2025-12-15 15:11:28 -0500

    updates

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -3,6 +3,7 @@ import { HeroSection } from './components/herosection';
 import WhyChooseUs from './components/whychooseus';
 import Solutions from './components/services';
 import { ContentSection } from './components/solutionscta';
+import Technology from './components/technology';
 import NewsAndResources from './components/newsandresources';
 import Footer from './components/footer';
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
         <WhyChooseUs />
         <Solutions />
         <ContentSection />
+        <Technology />
         <NewsAndResources />
         <Footer />
     </div>
