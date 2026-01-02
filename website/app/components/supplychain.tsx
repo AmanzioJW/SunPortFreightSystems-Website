f3d60376b89d9fbc730b9d2196097bb56cb734b6
AmanzioJW <richardanderson525@gmail.com>
2026-01-02 01:07:23 -0500

    UPDATES

--- website/app/components/supplychain.tsx
+++ website/app/components/supplychain.tsx
@@ -6,16 +6,22 @@ import SupplychainBackground from "./supplychain-components/supplychain-backgrou
 
 export default function Supplychain() {
     return (
-        <section className="u-pb-0">
+        <section className="track section u-pb-0">
             <div className="u-vflex-stretch-top u-vgap-64-24">
                 <div className="w-layout-blockcontainer container w-container">
                     <div className="row row-center-horizontal">
                         <div className="col col-lg-8">
-                            <h2 className="u-align-center">Your best people are drowning in the least valuable work</h2>
+                            <h2 className="u-align-center">Your best people are drowning in the least valuable work.</h2>
                         </div>
                     </div>
                 </div>
-                < SupplychainBackground />
+            <div className="tracks_container">
+                <div className="flex justify-center w-full">
+                    <div className="flex justify-center w-full">
+                        <SupplychainBackground />
+                    </div>
+                </div>
+            </div>
             </div>
         </section>
     );
