e861b84b167ffb1c4ca7d202d4d03683f5538ffb
AmanzioJW <richardanderson525@gmail.com>
2026-01-04 23:43:20 -0500

    updates

--- website/types/css-modules.d.ts
+++
@@ -1,7 +0,0 @@
-// TypeScript declarations for CSS Modules
-// Provides autocomplete and type safety for .module.css imports
-
-declare module '*.module.css' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
